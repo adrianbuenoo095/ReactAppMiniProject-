d308b8e56c96d14b2a58fad7bdf76f37521de9bf
Adrian <adrianbueno095@gmail.com>
2023-12-19 17:16:32 +0100

    add a lot of things

--- src/pages/ItemDetailsPage.jsx
+++ src/pages/ItemDetailsPage.jsx
@@ -33,7 +33,7 @@ const ItemDetailsPage = () => {
         </div>
       </div >
       <Link to="/">
-        <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
+        <button className="text-white px-4 py-2 rounded bg-red-500 hover:bg-green-600 transition duration-300 ease-in-out">
           Back
         </button>
       </Link>
