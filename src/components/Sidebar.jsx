b4573c5887e04086938db69d18149d200986eb3d
Adrian <adrianbueno095@gmail.com>
2023-12-20 10:45:38 +0100

    refactor Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,8 @@ import Navbar from "./Navbar";
 const Sidebar = () => {
 	const [nav, setNav] = useState();
 
+	console.log('nav', nav);
+
 	const toggleNav = () => {
 		setNav(!nav);
 	};
@@ -18,17 +20,18 @@ const Sidebar = () => {
 				</div>
 			</div>
 
-			{nav && <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>}
-
-			<div
-				className={`fixed top-0 left-0 w-[300px] h-screen bg-black z-10 duration-300 ${nav ? '' : 'left-[-100%]'
-					}`}
-			>
-				<FontAwesomeIcon onClick={toggleNav} icon={faX} className="absolute right-4 top-4 cursor-pointer" />
-				<div>
-					<Navbar />
+			{nav && <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0">
+				<div
+					className={`fixed top-0 left-0 w-[300px] h-screen bg-black z-10 duration-300 ${nav ? '' : 'left-[-100%]'
+						}`}
+				>
+					<FontAwesomeIcon onClick={toggleNav} icon={faX} className="absolute right-4 top-4 cursor-pointer" />
+					<div>
+						<Navbar />
+					</div>
 				</div>
-			</div>
+			</div>}
+
 		</div>
 	);
 }
